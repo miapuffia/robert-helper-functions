fbc26e1e8522ce89e84783a90c0a6cf1144153f6
Robert D. Rioja (miapuffia) <miapuffia@gmail.com>
2019-09-26 15:18:01 -0400

    Comments

--- NumberComparator.java
+++ NumberComparator.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+//This class provides a safe way to compare two Number objects
 @SuppressWarnings("unchecked")
 class NumberComparator implements Comparator<Number> {
 
